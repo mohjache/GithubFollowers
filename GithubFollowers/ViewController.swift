767bee407146c057fc1815eb7327d8d844a70f07
Anaru Herbert <anaru.herbert@ellumehealth.com>
2020-01-08 21:37:14 +1000

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Anaru Herbert on 8/1/20.
+//  Copyright © 2020 Anaru Herbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
